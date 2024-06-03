a6759817b702b021e082a7859485ee6468786849
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-06-03 15:37:43 +0200

    ADD: changind the route

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'verified'])
+->name('admin.')
+->prefix('admin')
+->group( function () {
+    Route::get('/admin', function () {
+        return view('admin.dashboard');
+    })->name('dashboard');
+
+    //ROUTE
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
